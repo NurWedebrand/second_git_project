0227c3e2fff324ed88d1f1406511bcc7a34d49e5
Aysenur Mercan <aysenurmercan98@gmail.com>
2025-03-12 12:15:46 -0500

    Update HelloNur.java

--- src/day00/HelloNur.java
+++ src/day00/HelloNur.java
@@ -3,5 +3,8 @@ package day00;
 public class HelloNur {
     public static void main(String[] args) {
         System.out.println("Hello Nur");
+         System.out.println("Hello Cydeo");
+         System.out.println("Hello Programmers");
+         
     }
 }
